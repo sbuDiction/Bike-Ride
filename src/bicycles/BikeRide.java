a238b731c807a05ea67a97f05132e80f8f294ccf
sbuDiction <nsbu68501@gmail.com>
2020-05-13 17:55:07 +0200

    second push

---
+++ src/bicycles/BikeRide.java
@@ -0,0 +1,17 @@
+package bicycles;
+
+public class BikeRide {
+        Bicycle bicycle = new Bicycle();
+        BikeRide bikeRide = new BikeRide(bicycle);
+        bikeRide.ride();
+        System.out.println(bikeRide.currentSpeed());
+
+    public void ride() {
+        this.bicycle.accelerate();
+        this.bicycle.accelerate();
+        this.bicycle.accelerate();
+        this.bicycle.brake();
+        this.bicycle.brake();
+        this.bicycle.accelerate();
+    }
+}
