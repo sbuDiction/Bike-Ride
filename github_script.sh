229724bc2b3a2150afdcf5c94544045ee361326f
sbuDiction <nsbu68501@gmail.com>
2020-06-10 13:05:56 +0200

    Automated commit

--- github_script.sh
+++ github_script.sh
@@ -4,14 +4,14 @@ function get_user_pass() {
   read -r -p "Enter a password " password
     if test "$password" = "codex123"
       then
-         echo "Password verified."
+         echo "Password verified now starting automated github push.........."
 #         check if github sever reachable than execute.
          timeout 5 ping github.com
          echo "Git hub sever working..."
          git status
          git add .
          git commit -m "Automated commit"
-#         git push origin master
+         git push origin master
          echo "Success....."
       else
          echo "Access denied."
